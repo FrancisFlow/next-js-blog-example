318043f27f13a89eb3113a0080e7005ebb4be4c8
francisflow <Iamngigikaranja1@aol.com>
2023-02-23 00:21:25 +0300

    Wow, this is cool

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,6 +4,14 @@ import Head from 'next/head';
 import Date from "../../components/date";
 import utilStyles from "../../styles/utilis.module.css";
 
+export async function getStaticPaths(){
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback : false
+    };
+}
+
 export async function getStaticProps({params}){
     const postData = await getPostData(params.id);
     return {
@@ -12,13 +20,6 @@ export async function getStaticProps({params}){
         },
     };
 
-}
-export async function getStaticPaths(){
-    const paths=getAllPostIds();
-    return {
-        paths,
-        fallback:false,
-    };
 }
 
 export default function Post({postData}){
